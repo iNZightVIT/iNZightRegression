78f8f61cc2578c5ef5b5a3b53cb78db2acb53fab
Tom Elliott <tomelliottnz@gmail.com>
2021-12-15 13:41:19 +1300

    Add message on startup (closes #62)

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(...) {
+    cat('*****************************************************************\n')
+    cat('* Loaded iNZightRegression\n*\n')
+    cat('* Methods imported from \'iNZightPlots\':\n')
+    cat('* - use `inzplot()` for diagnostic plots of model objects\n')
+    cat('* - use `inzsummary()` for a summary of model objects\n')
+    cat('*****************************************************************\n')
+}
